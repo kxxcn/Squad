2de45102e8933819b265791cb08df6f5d9740b1c
kxxcn <kxxcn.dev@gmail.com>
2018-04-27 13:37:47 +0900

    Add Interface BaseView, BasePresenter

---
+++ app/src/main/java/dev/kxxcn/app_squad/view/match/MatchContract.java
@@ -0,0 +1,18 @@
+package dev.kxxcn.app_squad.view.match;
+
+import dev.kxxcn.app_squad.view.BasePresenter;
+import dev.kxxcn.app_squad.view.BaseView;
+
+/**
+ * Created by Jun on 2018-04-27.
+ */
+
+public interface MatchContract {
+	interface View extends BaseView<MatchContract.Presenter> {
+
+	}
+
+	interface Presenter extends BasePresenter {
+
+	}
+}
