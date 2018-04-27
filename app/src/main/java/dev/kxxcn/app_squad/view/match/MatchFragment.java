71a0ba5d5c6168187bc952761566e2db1ddc967d
kxxcn <kxxcn.dev@gmail.com>
2018-04-27 13:35:15 +0900

    Add Floating Action Button

--- app/src/main/java/dev/kxxcn/app_squad/view/match/MatchFragment.java
+++ app/src/main/java/dev/kxxcn/app_squad/view/match/MatchFragment.java
@@ -2,6 +2,7 @@ package dev.kxxcn.app_squad.view.match;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,16 +18,29 @@ import dev.kxxcn.app_squad.R;
  * Created by Jun on 2018-04-26.
  */
 
-public class MatchFragment extends Fragment {
+public class MatchFragment extends Fragment implements MatchContract.View {
+
+	private MatchContract.Presenter mPresenter;
 
 	@BindView(R.id.vp_hic)
 	HorizontalInfiniteCycleViewPager horizontalInfiniteCycleViewPager;
+
+	@BindView(R.id.fab)
+	FloatingActionButton fab;
+
+	@Override
+	public void setPresenter(MatchContract.Presenter presenter) {
+		this.mPresenter = presenter;
+	}
 
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_match, container, false);
 		ButterKnife.bind(this, view);
+
+		new MatchPresenter(this);
+
 		return view;
 	}
 
