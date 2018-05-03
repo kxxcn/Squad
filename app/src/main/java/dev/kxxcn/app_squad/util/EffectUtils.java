673204751f3899925b3d77647d5274c636e82429
kxxcn <kxxcn.dev@gmail.com>
2018-05-03 13:09:56 +0900

    Add EffectUtils / UI Button & attrs.xml

---
+++ app/src/main/java/dev/kxxcn/app_squad/util/EffectUtils.java
@@ -0,0 +1,15 @@
+package dev.kxxcn.app_squad.util;
+
+import android.app.Activity;
+
+import dev.kxxcn.app_squad.R;
+
+/**
+ * Created by kxxcn on 2018-05-02.
+ */
+
+public class EffectUtils {
+	public static void fade(Activity activity) {
+		activity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+	}
+}
