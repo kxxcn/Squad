a1d77d633e6f4976adfce8555e81c83da1b5075c
kxxcn <kxxcn.dev@gmail.com>
2018-05-04 12:16:08 +0900

    Add Sign Up Activity

---
+++ app/src/main/java/dev/kxxcn/app_squad/ui/signup/SignupActivity.java
@@ -0,0 +1,18 @@
+package dev.kxxcn.app_squad.ui.signup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kxxcn on 2018-05-04.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
---
+++ app/src/main/java/dev/kxxcn/app_squad/ui/signup/SignupContract.java
@@ -0,0 +1,8 @@
+package dev.kxxcn.app_squad.ui.signup;
+
+/**
+ * Created by kxxcn on 2018-05-04.
+ */
+
+public interface SignupContract {
+}
