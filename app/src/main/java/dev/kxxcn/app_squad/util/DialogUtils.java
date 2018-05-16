c6e15c97903a983c2a8727d86d56a4457fdff449
kxxcn <kxxcn.dev@gmail.com>
2018-05-16 17:34:14 +0900

    Add DialogUtils

---
+++ app/src/main/java/dev/kxxcn/app_squad/util/DialogUtils.java
@@ -0,0 +1,48 @@
+package dev.kxxcn.app_squad.util;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.os.Build;
+import android.view.ContextThemeWrapper;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by kxxcn on 2018-05-16.
+ */
+
+public class DialogUtils {
+	public static void showDatePickerDialog(Context context, DatePickerDialog.OnDateSetListener dateSetListener) {
+		Calendar calendar = Calendar.getInstance();
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		Context instance = new ContextThemeWrapper(context, android.R.style.Theme_Holo_Light_Dialog);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+			// API 24 이상일 경우 시스템 기본 테마 사용
+			instance = context;
+		}
+		DatePickerDialog datePickerDialog = new DatePickerDialog(instance, dateSetListener, year, month, day);
+		datePickerDialog.getDatePicker().setMinDate(calendar.getTime().getTime());
+		datePickerDialog.show();
+	}
+
+	public static void showTimePickerDialog(Context context, TimePickerDialog.OnTimeSetListener timeSetListener) {
+		Date hour = new Date();
+		String hourOfDay = new SimpleDateFormat("HH", Locale.KOREA).format(hour);
+		Date min = new Date();
+		String minute = new SimpleDateFormat("mm", Locale.KOREA).format(min);
+		Context instance = new ContextThemeWrapper(context, android.R.style.Theme_Holo_Light_Dialog);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+			// API 24 이상일 경우 시스템 기본 테마 사용
+			instance = context;
+		}
+		TimePickerDialog timePickerDialog = new TimePickerDialog(instance, timeSetListener,
+				Integer.parseInt(hourOfDay), Integer.parseInt(minute), false);
+		timePickerDialog.show();
+	}
+}
