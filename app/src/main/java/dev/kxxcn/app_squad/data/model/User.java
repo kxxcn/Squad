a70b9a62e07952368184494d07924d1db0e807a1
kxxcn <kxxcn.dev@gmail.com>
2018-06-11 14:28:53 +0900

    Add field token.

--- app/src/main/java/dev/kxxcn/app_squad/data/model/User.java
+++ app/src/main/java/dev/kxxcn/app_squad/data/model/User.java
@@ -9,39 +9,33 @@ public class User {
 	private String email;
 	private String uid;
 	private String team;
+	private String token;
 
 	public User() {
 
 	}
 
-	public User(String email, String uid, String team) {
+	public User(String email, String uid, String team, String token) {
 		this.email = email;
 		this.uid = uid;
 		this.team = team;
+		this.token = token;
 	}
 
 	public String getEmail() {
 		return email;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 	public String getUid() {
 		return uid;
-	}
-
-	public void setUid(String uid) {
-		this.uid = uid;
 	}
 
 	public String getTeam() {
 		return team;
 	}
 
-	public void setTeam(String team) {
-		this.team = team;
+	public String getToken() {
+		return token;
 	}
 
 }
