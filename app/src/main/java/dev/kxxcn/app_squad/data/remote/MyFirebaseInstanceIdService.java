47ca3bead87a3de61d9b43424576cdd0d7cd27c1
kxxcn <kxxcn.dev@gmail.com>
2018-06-12 14:29:13 +0900

    Add logic what save it when the token is refreshed.

--- app/src/main/java/dev/kxxcn/app_squad/data/remote/MyFirebaseInstanceIdService.java
+++ app/src/main/java/dev/kxxcn/app_squad/data/remote/MyFirebaseInstanceIdService.java
@@ -1,7 +1,13 @@
 package dev.kxxcn.app_squad.data.remote;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by kxxcn on 2018-06-11.
@@ -9,13 +15,20 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
 
 public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
 
+	private static final String COLLECTION_NAME_USER = "user";
+
+	private static final String TOKEN = "token";
+
 	@Override
 	public void onTokenRefresh() {
 		saveNewToken(FirebaseInstanceId.getInstance().getToken());
 	}
 
 	private void saveNewToken(String token) {
-
+		Map<String, Object> task = new HashMap<>();
+		task.put(TOKEN, token);
+		DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+		reference.child(COLLECTION_NAME_USER + "/" + FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(task);
 	}
 
 }
