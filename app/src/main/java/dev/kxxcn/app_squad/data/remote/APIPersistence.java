75da9304904b39ec7efcef0bb252c66133774d01
kxxcn <kxxcn.dev@gmail.com>
2018-06-21 16:31:51 +0900

    Move the variable position.

--- app/src/main/java/dev/kxxcn/app_squad/data/remote/APIPersistence.java
+++ app/src/main/java/dev/kxxcn/app_squad/data/remote/APIPersistence.java
@@ -5,12 +5,7 @@ package dev.kxxcn.app_squad.data.remote;
  */
 
 public class APIPersistence {
-
 	public static final String FCM_SERVER_URL = "https://fcm.googleapis.com/";
 	public static final String FCM_SERVER_KEY = "AAAAogOTo-4:APA91bEJaeSUvNeumE60WxDyc96lzl-wRck3tWw-i6g4j0_EAk2_IKv7lPWUmh2DQ5xP6pYTg-LMzac4MGtxM9PHzl6f43i7ObCTHbeIxBwIibATL2JIYIZ0B_EpyBPBOq6eTI4onSPc";
-
-	public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-
 	public static final String SEND_MESSAGE = "fcm/send";
-
 }
