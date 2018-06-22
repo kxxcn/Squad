bf9bf52e28bf2c9c45bdc676566489c50fb325b5
kxxcn <kxxcn.dev@gmail.com>
2018-06-22 17:59:30 +0900

    Modify name of class.

--- app/src/main/java/dev/kxxcn/app_squad/ui/main/team/LoadAdapter.java
+++ app/src/main/java/dev/kxxcn/app_squad/ui/main/team/LoadAdapter.java
@@ -15,21 +15,18 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import dev.kxxcn.app_squad.R;
 import dev.kxxcn.app_squad.data.model.Notification;
-import dev.kxxcn.app_squad.util.DialogUtils;
-
-import static dev.kxxcn.app_squad.util.Constants.TYPE_COLLECTION;
 
 /**
  * Created by kxxcn on 2018-06-14.
  */
 
-public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
+public class LoadAdapter extends RecyclerView.Adapter<LoadAdapter.ViewHolder> {
 
 	private List<Notification> mList;
 
 	private TeamContract.ItemClickListener mItemClickListener;
 
-	public NotificationAdapter(List<Notification> list, TeamContract.ItemClickListener itemClickListener) {
+	public LoadAdapter(List<Notification> list, TeamContract.ItemClickListener itemClickListener) {
 		this.mList = list;
 		this.mItemClickListener = itemClickListener;
 	}
@@ -37,26 +34,17 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
 	@Override
 	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_notification, parent, false);
-		return new NotificationAdapter.ViewHolder(view, mItemClickListener);
+		return new LoadAdapter.ViewHolder(view, mItemClickListener);
 	}
 
 	@Override
 	public void onBindViewHolder(ViewHolder holder, int position) {
 		holder.tv_message.setText(mList.get(holder.getAdapterPosition()).getMessage());
+		holder.tv_time.setText(mList.get(holder.getAdapterPosition()).getTimestamp());
 		if (mList.get(holder.getAdapterPosition()).getSender().equals(R.string.app_name)) {
 			holder.ll_root.setOnClickListener(null);
 			holder.ib_arrow.setVisibility(View.GONE);
 			holder.iv_icon.setBackgroundResource(R.drawable.ic_admin);
-		}
-
-		if (Integer.parseInt(DialogUtils.getFormattedDate(DialogUtils.getDate(), TYPE_COLLECTION))
-				> Integer.parseInt(mList.get(holder.getAdapterPosition()).getDate())) {
-			holder.ll_root.setOnClickListener(null);
-			holder.ib_arrow.setVisibility(View.GONE);
-			int index = mList.get(holder.getAdapterPosition()).getTimestamp().indexOf(" ");
-			holder.tv_time.setText(mList.get(holder.getAdapterPosition()).getTimestamp().substring(0, index));
-		} else {
-			holder.tv_time.setText(mList.get(holder.getAdapterPosition()).getTimestamp());
 		}
 	}
 
