26af85fdec02837933cd68a2fe5f5fc5ed8137f1
kxxcn <kxxcn.dev@gmail.com>
2018-07-04 15:17:55 +0900

    Add constants.

--- app/src/main/java/dev/kxxcn/app_squad/util/Constants.java
+++ app/src/main/java/dev/kxxcn/app_squad/util/Constants.java
@@ -21,6 +21,9 @@ public class Constants {
 	public static final int TYPE_COLLECTION = 0;
 	public static final int TYPE_SORT = 1;
 	public static final int VIBRATE_NOTIFICATION = 1000;
+	public static final int DISMISS_NOTI_DIALOG = 1500;
+	public static final int TYPE_REQUEST = 0;
+	public static final int TYPE_RESPONSE = 1;
 
 	public enum ListsFilterType {
 		MATCH_LIST,
