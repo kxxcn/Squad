bb2003d02a1b6c9d39aaedb4034d10bc58b0ce0e
kxxcn <kxxcn.dev@gmail.com>
2018-07-04 15:16:50 +0900

    Add a type constants.

--- app/src/main/java/dev/kxxcn/app_squad/data/remote/APIPersistence.java
+++ app/src/main/java/dev/kxxcn/app_squad/data/remote/APIPersistence.java
@@ -6,6 +6,8 @@ package dev.kxxcn.app_squad.data.remote;
 
 public class APIPersistence {
 	public static final String FCM_SERVER_URL = "https://fcm.googleapis.com/";
-	public static final String FCM_SERVER_KEY = "AAAAogOTo-4:APA91bEJaeSUvNeumE60WxDyc96lzl-wRck3tWw-i6g4j0_EAk2_IKv7lPWUmh2DQ5xP6pYTg-LMzac4MGtxM9PHzl6f43i7ObCTHbeIxBwIibATL2JIYIZ0B_EpyBPBOq6eTI4onSPc";
+	public static final String FCM_SERVER_KEY = "AAAAogOTo-4:APA91bHZuclpmakqM5flUjj_3HPsVF-kAsvQV8PgClPO7Nv0qHdHO8HgyuxokCKDsQC_4TIN9p8C0gTWOE6x9aXJaRRCZtr_Ly7_UQKPBrOnn5HcblIzFtBmfvj4HMTk-bUJxoeh8CosvXY4zwQgZW7svETLIpDVQA";
 	public static final String SEND_MESSAGE = "fcm/send";
+	public static final String TYPE_REQUEST = "0";
+	public static final String TYPE_RESPONSE = "1";
 }
