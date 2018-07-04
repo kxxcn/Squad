9468468ee844d2ed8e1a9a8c84e5c38a7682bb52
kxxcn <kxxcn.dev@gmail.com>
2018-07-04 17:18:03 +0900

    Add a background to the card view.

--- app/src/main/java/dev/kxxcn/app_squad/ui/main/team/TeamAdapter.java
+++ app/src/main/java/dev/kxxcn/app_squad/ui/main/team/TeamAdapter.java
@@ -5,7 +5,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.List;
 
@@ -37,6 +41,7 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.ViewHolder> {
 
 	@Override
 	public void onBindViewHolder(ViewHolder holder, int position) {
+		Glide.with(mContext).load(R.drawable.ic_team_background).diskCacheStrategy(DiskCacheStrategy.NONE).into(holder.iv_background);
 		holder.tv_date.setText(getFormattedDate(mList.get(holder.getAdapterPosition()).getDate()));
 		holder.tv_place.setText(mList.get(holder.getAdapterPosition()).getPlace());
 		if (mList.get(holder.getAdapterPosition()).isHome()) {
@@ -54,6 +59,9 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.ViewHolder> {
 	}
 
 	static class ViewHolder extends RecyclerView.ViewHolder {
+		@BindView(R.id.iv_background)
+		ImageView iv_background;
+
 		@BindView(R.id.tv_date)
 		TextView tv_date;
 		@BindView(R.id.tv_place)
