115ec79ecfdf8e3fbf9ce500a2b23a3dd3867b52
kxxcn <kxxcn.dev@gmail.com>
2018-07-19 18:03:56 +0900

    Add dialog fragment "PlayerListDialog".

---
+++ app/src/main/java/dev/kxxcn/app_squad/ui/main/list/playerlist/PlayerListDialog.java
@@ -0,0 +1,10 @@
+package dev.kxxcn.app_squad.ui.main.list.playerlist;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by kxxcn on 2018-07-19.
+ */
+
+public class PlayerListDialog extends DialogFragment {
+}
