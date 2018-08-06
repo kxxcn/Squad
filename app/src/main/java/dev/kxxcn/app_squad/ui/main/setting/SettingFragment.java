3d2565ae54ceb2f422f1472b75699e18b2a156d8
kxxcn <kxxcn.dev@gmail.com>
2018-08-06 17:05:19 +0900

    Add a click event handling function.

--- app/src/main/java/dev/kxxcn/app_squad/ui/main/setting/SettingFragment.java
+++ app/src/main/java/dev/kxxcn/app_squad/ui/main/setting/SettingFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
@@ -108,6 +109,16 @@ public class SettingFragment extends Fragment implements SettingContract.View {
 		DialogFragment newFragment = ProfileDialog.newInstance();
 		newFragment.show(getChildFragmentManager(), DIALOG_FRAGMENT);
 	}
+
+	@OnClick(R.id.ll_notice)
+	public void showNotice() {
+		Toast.makeText(getContext(), getString(R.string.coming_soon), Toast.LENGTH_SHORT).show();
+	}
+
+	@OnClick(R.id.ll_out)
+	public void onOut() {
+	}
+
 
 	public static Fragment newInstance() {
 		return new SettingFragment();
